06ff7f11de4d05bae6d33180814770a177067a00
MahimaSingh77 <mahima2454.be21@chitkara.edu.in>
2023-02-18 22:11:58 +0530

    Multidimensional

---
+++ multidimensionalArray/1problem.c
@@ -0,0 +1,18 @@
+/*
+Accessing multidimensional array
+*/
+
+#include<stdio.h>
+int main(){
+
+    int mat[3][2]={{10,20},{30,40},{50,60}};
+
+    for (int i=0 ; i<3; i++){
+        for(int j=0; j<2; j++){
+            printf("%d " , mat[i][j]);
+        }
+    }
+    return 0;
+}
+
+
---
+++ multidimensionalArray/2problem.c
@@ -0,0 +1,14 @@
+/*
+Passing 2D arrays as arguments to functions
+*/
+#include<stdio.h>
+void print(int m , int n , int mat[][n]  ){
+    for(int i=0; i<m; i++)
+    for(int j=0; j<n; j++)
+    printf("%d " , mat[i][j]);
+}
+int main(){
+    int mat[3][2] = {{10,20} , {30,40} , {50,60}};
+    print(  3 , 2  , mat );
+    return 0;
+}
